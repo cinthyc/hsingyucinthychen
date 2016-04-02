1f3c02a275be0197dd19986f1ec1d383d4d456f6
cinthyc <hychen71@bu.edu>
2016-04-02 12:19:36 -0400

    JPG change

--- js/boston.js
+++ js/boston.js
@@ -1,4 +1,4 @@
-var images = ["../images/boston/DSC00899.jpg", "../images/boston/DSC00965.jpg", "../images/boston/IMG_1547.jpg", "../images/boston/IMG_1369.jpg"];
+var images = ["../images/boston/DSC00899.JPG", "../images/boston/DSC00965.JPG", "../images/boston/IMG_1547.JPG", "../images/boston/IMG_1369.JPG"];
 var count = 0
 
 $("#next").on("click",function(){
