a92dcd76b7307de5561de26d4b842e3576750594
cinthyc <hychen71@bu.edu>
2016-04-19 12:03:38 -0400

    Update venice.js

--- js/venice.js
+++ js/venice.js
@@ -1,4 +1,4 @@
-var images = ["images/venice/dsc03365.jpg", "images/venice/dsc03232.jpg", "images/venice/dsc03324.jpg", "images/venice/dsc03202.jpg"];
+var images = ["images/venice/DSC03365.JPG", "images/venice/DSC03232.JPG", "images/venice/DSC03324.JPG", "images/venice/DSC03202.JPG"];
 var count = 0
 
 $("#next").on("click",function(){
